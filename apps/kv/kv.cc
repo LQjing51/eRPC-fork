5643077dd7747be25d406f9a9ccbe18f3a8abbe3
qijing <liqijing19@mails.ucas.ac.cn>
2024-10-31 23:15:06 +0800

    add naive kv store

---
+++ apps/kv/kv.cc
@@ -0,0 +1,316 @@
+#include "kv.h"
+#include <signal.h>
+#include <cstring>
+#include "util/autorun_helpers.h"
+
+void app_cont_func(void *, void *);  // Forward declaration
+
+static constexpr bool kAppVerbose = false;
+
+void get_req_handler(erpc::ReqHandle *req_handle, void *_context) {
+  auto *c = static_cast<AppContext *>(_context);
+
+  const size_t etid = c->rpc_->get_etid();
+
+  KV *kv = c->server.kv;
+  assert(kv != nullptr);
+
+  const auto *req_msgbuf = req_handle->get_req_msgbuf();
+  assert(req_msgbuf->get_data_size() == sizeof(wire_req_t));
+
+  auto *req = reinterpret_cast<wire_req_t *>(req_msgbuf->buf_);
+
+  erpc::Rpc<erpc::CTransport>::resize_msg_buffer(&req_handle->pre_resp_msgbuf_,
+                                                sizeof(wire_resp_t));
+  auto *resp =
+      reinterpret_cast<wire_resp_t *>(req_handle->pre_resp_msgbuf_.buf_);
+  memset(&(resp->value), 0, KV::kValueSize);
+  const bool success = kv->get(req->get_req.key, &(resp->value));
+  resp->resp_type = success ? RespType::Success : RespType::Failure;
+  printf("%d, key %ld value: %ld\n", success, *reinterpret_cast<size_t*>(req->get_req.key.key_), *reinterpret_cast<size_t*>(resp->value.value_));
+
+  if (kAppVerbose) {
+    printf(
+        "main: Handled point request in eRPC thread %zu. Key %s, found %s, "
+        "value %s\n",
+        etid, req->to_string().c_str(), success ? "yes" : "no",
+        success ? resp->to_string().c_str() : "N/A");
+  }
+
+  c->rpc_->enqueue_response(req_handle, &req_handle->pre_resp_msgbuf_);
+}
+
+void put_req_handler(erpc::ReqHandle *req_handle, void *_context) {
+  auto *c = static_cast<AppContext *>(_context);
+
+  const size_t etid = c->rpc_->get_etid();
+
+  KV *kv = c->server.kv;
+  assert(kv != nullptr);
+
+  const auto *req_msgbuf = req_handle->get_req_msgbuf();
+  assert(req_msgbuf->get_data_size() == sizeof(wire_req_t));
+
+  auto *req = reinterpret_cast<const wire_req_t *>(req_msgbuf->buf_);
+
+  erpc::Rpc<erpc::CTransport>::resize_msg_buffer(&req_handle->pre_resp_msgbuf_,
+                                                 sizeof(wire_resp_t));
+  auto *resp =
+      reinterpret_cast<wire_resp_t *>(req_handle->pre_resp_msgbuf_.buf_);
+  //TODO QIJING
+  const bool success = kv->put(req->put_req.key, req->put_req.value);
+  resp->resp_type = success ? RespType::Success : RespType::Failure;
+
+  if (kAppVerbose) {
+    printf("main: Handling range request in eRPC thread %zu.\n", etid);
+  }
+
+  c->rpc_->enqueue_response(req_handle, &req_handle->pre_resp_msgbuf_);
+}
+
+// Send one request using this MsgBuffer
+void send_req(AppContext *c, size_t msgbuf_idx) {
+  erpc::MsgBuffer &req_msgbuf = c->client.window_[msgbuf_idx].req_msgbuf_;
+  assert(req_msgbuf.get_data_size() == sizeof(wire_req_t));
+
+  // Generate a random request
+  wire_req_t req;
+
+  if (c->fastrand_.next_u32() % 100 < FLAGS_get_req_percent) {
+    if (c->fastrand_.next_u32() % 100 < FLAGS_get_fg_percent) {
+      req.req_type = kAppGetFgType;
+    } else {
+      req.req_type = kAppGetBgType;
+    }
+    /*
+    ensure the key size is larger or equal to 4 Bytes.
+    choose key randomly, key range is [0, FLAGS_num_keys].
+    Only Map[0-FLAGS_num_keys][] will be touched no matter key size.
+    */
+    memset(&(req.get_req.key.key_), 0, KV::kKeySize);
+    uint32_t *key_32 = reinterpret_cast<uint32_t *>(req.get_req.key.key_);
+    *key_32 = c->fastrand_.next_u32() % FLAGS_num_keys;
+  } else {
+    if (c->fastrand_.next_u32() % 100 < FLAGS_put_fg_percent) {
+      req.req_type = kAppPutFgType;
+    } else {
+      req.req_type = kAppPutBgType;
+    }
+
+    /*
+    ensure the key size is larger or equal to 4 Bytes.
+    choose key randomly, key range is [0, FLAGS_num_keys].
+    Only Map[0-FLAGS_num_keys][] will be touched no matter key size.
+    */
+    memset(&(req.put_req.key.key_), 0, KV::kKeySize);
+    uint32_t *key_32 = reinterpret_cast<uint32_t *>(req.put_req.key.key_);
+    *key_32 = c->fastrand_.next_u32() % FLAGS_num_keys;
+    
+    /*ensure the value size is larger or equal to 8 Bytes.*/
+    memset(&(req.put_req.value.value_), 0, KV::kValueSize);
+    uint64_t *value_64 = reinterpret_cast<uint64_t *>(req.put_req.value.value_);
+    *value_64 = (*key_32)*0x12345+0x10501;
+  }
+
+  *reinterpret_cast<wire_req_t *>(req_msgbuf.buf_) = req;
+
+  c->client.window_[msgbuf_idx].req_key = (req.req_type == kAppGetFgType || req.req_type == kAppGetBgType)? req.get_req.key : req.put_req.key;
+  c->client.window_[msgbuf_idx].req_ts_ = erpc::rdtsc();
+
+  if (kAppVerbose) {
+    printf("main: Enqueuing request with msgbuf_idx %zu.\n", msgbuf_idx);
+    sleep(1);
+  }
+
+  c->rpc_->enqueue_request(0, req.req_type, &req_msgbuf,
+                           &c->client.window_[msgbuf_idx].resp_msgbuf_,
+                           app_cont_func, reinterpret_cast<void *>(msgbuf_idx));
+}
+
+void app_cont_func(void *_context, void *_msgbuf_idx) {
+  auto *c = static_cast<AppContext *>(_context);
+  const auto msgbuf_idx = reinterpret_cast<size_t>(_msgbuf_idx);
+  if (kAppVerbose) {
+    printf("main: Received response for msgbuf %zu.\n", msgbuf_idx);
+  }
+
+  const auto &resp_msgbuf = c->client.window_[msgbuf_idx].resp_msgbuf_;
+  erpc::rt_assert(resp_msgbuf.get_data_size() == sizeof(wire_resp_t),
+                  "Invalid response size");
+
+  const double usec =
+      erpc::to_usec(erpc::rdtsc() - c->client.window_[msgbuf_idx].req_ts_,
+                    c->rpc_->get_freq_ghz());
+  assert(usec >= 0);
+
+  const auto *req = reinterpret_cast<wire_req_t *>(
+      c->client.window_[msgbuf_idx].req_msgbuf_.buf_);
+
+  if (req->req_type == kAppGetFgType || req->req_type == kAppGetBgType) {
+    c->client.get_latency.update(static_cast<size_t>(usec * 10.0));  // < 1us
+
+    {
+      const auto *wire_resp = reinterpret_cast<wire_resp_t *>(resp_msgbuf.buf_);
+      const uint64_t* recvd_value = reinterpret_cast<const uint64_t *>(wire_resp->value.value_);
+      const uint32_t* key = reinterpret_cast<const uint32_t *>(c->client.window_[msgbuf_idx].req_key.key_);
+      if ((static_cast<uint64_t>(*key)*0x12345+0x10501) != *recvd_value) {
+        fprintf(stderr,
+                "KV Get Value mismatch. Req key = %u, recvd_value = %lu\n",
+                *key, *recvd_value);
+      }
+    }
+  } else {
+    c->client.put_latency.update(static_cast<size_t>(usec * 10.0));
+    // Qijing TODO: add success check?
+  }
+
+  c->client.num_resps_tot++;
+  send_req(c, msgbuf_idx);
+}
+
+void client_print_stats(AppContext &c) {
+  const double seconds = c.client.tput_timer.get_us() / 1e6;
+  const double tput_mrps = c.client.num_resps_tot / (seconds * 1000000);
+  app_stats_t &stats = c.client.app_stats[c.thread_id_];
+  stats.mrps = tput_mrps;
+  stats.lat_us_50 = c.client.get_latency.perc(0.50) / 10.0;
+  stats.lat_us_90 = c.client.get_latency.perc(0.90) / 10.0;
+  stats.lat_us_99 = c.client.get_latency.perc(0.99) / 10.0;
+
+  printf(
+      "Client %zu. Tput = %.3f Mrps. "
+      "Get-query latency (us) = {%.1f 50th, %.1f 90th, %.1f 99th}. "
+      "Put-query latency (us) = {%.1f 50th, %.1f 90th, %.1f 99th}.\n",
+      c.thread_id_, tput_mrps, stats.lat_us_50, stats.lat_us_90,
+      stats.lat_us_99, c.client.put_latency.perc(.5)/10.0, c.client.put_latency.perc(.9)/10.0, c.client.put_latency.perc(.99)/10.0);
+
+  c.client.num_resps_tot = 0;
+  c.client.get_latency.reset();
+  c.client.put_latency.reset();
+
+  c.client.tput_timer.reset();
+}
+
+void client_thread_func(size_t thread_id, app_stats_t *app_stats,
+                        erpc::Nexus *nexus) {
+  AppContext c;
+  c.thread_id_ = thread_id;
+  c.client.app_stats = app_stats;
+
+  std::vector<size_t> port_vec = flags_get_numa_ports(FLAGS_numa_node);
+  erpc::rt_assert(port_vec.size() > 0);
+  const uint8_t phy_port = port_vec.at(thread_id % port_vec.size());
+
+  erpc::Rpc<erpc::CTransport> rpc(nexus, static_cast<void *>(&c),
+                                  static_cast<uint8_t>(thread_id),
+                                  basic_sm_handler, phy_port);
+  rpc.retry_connect_on_invalid_rpc_id_ = true;
+  c.rpc_ = &rpc;
+
+  // Each client creates a session to only one server thread
+  const size_t client_gid =
+      (FLAGS_process_id * FLAGS_num_client_threads) + thread_id;
+  const size_t server_tid =
+      client_gid % FLAGS_num_server_fg_threads;  // eRPC TID
+
+  c.session_num_vec_.resize(1);
+  c.session_num_vec_[0] =
+      rpc.create_session(erpc::get_uri_for_process(0), server_tid);
+  assert(c.session_num_vec_[0] >= 0);
+
+  while (c.num_sm_resps_ != 1) {
+    rpc.run_event_loop(200);  // 200 milliseconds
+    if (ctrl_c_pressed == 1) return;
+  }
+  assert(c.rpc_->is_connected(c.session_num_vec_[0]));
+  fprintf(stderr, "main: Thread %zu: Connected. Sending requests.\n",
+          thread_id);
+
+  alloc_req_resp_msg_buffers(&c);
+  c.client.tput_timer.reset();
+  for (size_t i = 0; i < FLAGS_req_window; i++) send_req(&c, i);
+
+  for (size_t i = 0; i < FLAGS_test_ms; i += kAppEvLoopMs) {
+    c.rpc_->run_event_loop(kAppEvLoopMs);
+    if (ctrl_c_pressed == 1) break;
+    client_print_stats(c);
+  }
+}
+
+void server_thread_func(size_t thread_id, erpc::Nexus *nexus) {
+  AppContext c;
+  c.thread_id_ = thread_id;
+
+  KV* kv = new KV(FLAGS_num_keys);
+  c.server.kv = kv;
+  printf("finished initial KV\n");
+
+  std::vector<size_t> port_vec = flags_get_numa_ports(FLAGS_numa_node);
+  erpc::rt_assert(port_vec.size() > 0);
+  uint8_t phy_port = port_vec.at(thread_id % port_vec.size());
+
+  erpc::Rpc<erpc::CTransport> rpc(nexus, static_cast<void *>(&c),
+                                  static_cast<uint8_t>(thread_id),
+                                  basic_sm_handler, phy_port);
+  c.rpc_ = &rpc;
+  while (ctrl_c_pressed == 0) rpc.run_event_loop(200);
+}
+
+int main(int argc, char **argv) {
+  signal(SIGINT, ctrl_c_handler);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  erpc::rt_assert(FLAGS_req_window <= kAppMaxReqWindow, "Invalid req window");
+  erpc::rt_assert(FLAGS_get_req_percent <= 100, "Invalid get req percent");
+
+  if (FLAGS_num_server_bg_threads == 0) {
+    printf(
+        "main: Warning: No background threads. "
+        "Range queries will run in foreground.\n");
+  }
+
+  if (is_server()) {
+    erpc::rt_assert(FLAGS_process_id == 0, "Invalid server process ID");
+
+    // eRPC stuff
+    erpc::Nexus nexus(erpc::get_uri_for_process(FLAGS_process_id),
+                      FLAGS_numa_node, FLAGS_num_server_bg_threads);
+
+    if ((FLAGS_get_fg_percent != 100) || (FLAGS_get_fg_percent != 100)) {
+      erpc::rt_assert(FLAGS_num_server_bg_threads > 0, "Need bg threads to handle bg requests");
+    }
+    
+    nexus.register_req_func(kAppGetFgType, get_req_handler,
+                            erpc::ReqFuncType::kForeground);
+
+    nexus.register_req_func(kAppPutFgType, put_req_handler,
+                            erpc::ReqFuncType::kForeground);
+
+    if (FLAGS_num_server_bg_threads > 0) {
+      nexus.register_req_func(kAppGetBgType, get_req_handler,
+                            erpc::ReqFuncType::kBackground);
+
+      nexus.register_req_func(kAppPutBgType, put_req_handler,
+                            erpc::ReqFuncType::kBackground);
+    }
+    std::vector<std::thread> thread_arr(FLAGS_num_server_fg_threads);
+    for (size_t i = 0; i < FLAGS_num_server_fg_threads; i++) {
+      thread_arr[i] = std::thread(server_thread_func, i, &nexus);
+      erpc::bind_to_core(thread_arr[i], FLAGS_numa_node, i);
+    }
+
+    for (auto &thread : thread_arr) thread.join();
+  } else {
+    erpc::rt_assert(FLAGS_process_id > 0, "Invalid process ID");
+    erpc::Nexus nexus(erpc::get_uri_for_process(FLAGS_process_id),
+                      FLAGS_numa_node, FLAGS_num_server_bg_threads);
+
+    std::vector<std::thread> thread_arr(FLAGS_num_client_threads);
+    auto *app_stats = new app_stats_t[FLAGS_num_client_threads];
+    for (size_t i = 0; i < FLAGS_num_client_threads; i++) {
+      thread_arr[i] = std::thread(client_thread_func, i, app_stats, &nexus);
+      erpc::bind_to_core(thread_arr[i], FLAGS_numa_node, i);
+    }
+
+    for (auto &thread : thread_arr) thread.join();
+  }
+}
---
+++ apps/kv/kv.h
@@ -0,0 +1,244 @@
+#ifndef KV_H
+#define KV_H
+
+#include <gflags/gflags.h>
+#include <signal.h>
+#include <unordered_map>
+#include <optional>
+#include "../apps_common.h"
+#include "rpc.h"
+#include "util/latency.h"
+#include "util/numautils.h"
+#include "util/timer.h"
+
+static constexpr size_t kAppGetFgType = 1;
+static constexpr size_t kAppGetBgType = 2;
+static constexpr size_t kAppPutFgType = 3;
+static constexpr size_t kAppPutBgType = 4;
+static constexpr size_t kAppEvLoopMs = 500;
+
+static constexpr size_t kAppMaxReqWindow = 256;  // Max pending reqs per client
+
+// Globals
+volatile sig_atomic_t ctrl_c_pressed = 0;
+void ctrl_c_handler(int) { ctrl_c_pressed = 1; }
+
+// Flags
+DEFINE_uint64(num_server_fg_threads, 0, "Number of server foreground threads");
+DEFINE_uint64(num_server_bg_threads, 0, "Number of server background threads");
+DEFINE_uint64(num_client_threads, 0, "Number of client threads");
+DEFINE_uint64(req_window, 0, "Outstanding requests per client thread");
+DEFINE_uint64(num_keys, 0, "Number of keys in the server's Hashmap");
+DEFINE_uint64(get_req_percent, 0, "Percentage of get");
+DEFINE_uint64(get_fg_percent, 0, "Percentage of get requests allocated to fg threads");
+DEFINE_uint64(put_fg_percent, 0, "Percentage of put requests allocated to fg threads");
+
+// Return true iff this machine is the one server
+bool is_server() { return FLAGS_process_id == 0; }
+/*
+req data size = size_t + kKeySize + kValueSize = 88 bytes
+resp data size = size_t + kValueSize = 72 bytes
+
+req/resp pkt size : key size : value size
+64                :
+128               :
+256               :
+512               :
+1024              : 
+*/
+class KV {
+public:
+  static constexpr size_t kKeySize = 16;
+  static constexpr size_t kValueSize = 64;
+
+  typedef struct {
+    uint8_t key_[kKeySize];
+  } key_t;
+  
+  typedef struct {
+    uint8_t value_[kValueSize];
+  } value_t;
+
+  KV(size_t initial_size) {
+    for(size_t i = 0; i < initial_size; i++){
+      key_t key;
+      for(size_t t = 0; t < kKeySize; t++) {
+        key.key_[t] = i & ((1<<8) - 1);
+        i >>= 8;
+      }
+      value_t value;
+      size_t v = i*0x12345+0x10501;
+      for(size_t t = 0; t < kValueSize; t++) {
+        value.value_[t] = v & ((1<<8) - 1);
+        v >>= 8;
+      }
+      put(key, value);
+    }
+  }
+
+  ~KV() {}
+
+  bool put(key_t key, value_t value) {
+    kvmap[key] = value;
+    return true;
+  }
+
+  bool get(key_t key, value_t* value) {
+    auto it = kvmap.find(key);
+
+    if (it != kvmap.end()) {
+      memcpy(value->value_, &(it->second), KV::kValueSize);
+      return true;
+    }
+
+    return false;
+  }
+
+private:
+  struct HashFunc {
+    std::size_t operator()(const key_t &key) const {
+      std::size_t hash = 0;
+      for (size_t i = 0; i < kKeySize; i++) {
+        hash = hash * 271 + key.key_[i];
+      }
+      return hash;
+    }
+  };
+  struct CompareFunc {
+    bool operator()(const key_t &key1, const key_t &key2) const {
+      for(size_t i = 0; i < kKeySize; i++){
+        if(key1.key_[i] != key2.key_[i]) return false;
+      }
+      return true;
+    }
+  };
+  std::unordered_map<key_t, value_t, HashFunc, CompareFunc> kvmap;
+
+};
+
+// req size = size_t + kKeySize + kValueSize = 88 bytes
+struct wire_req_t {
+  size_t req_type;//kAppGetType or kAppPutType
+  union {
+    struct {
+      KV::key_t key;
+    } get_req;
+
+    struct {
+      KV::key_t key;
+      KV::value_t value;
+    } put_req;
+  };
+
+  std::string to_string() const {
+    std::ostringstream ret;
+    ret << "[Type " << ((req_type == kAppGetFgType || req_type == kAppGetBgType) ? "get" : "put")
+        << ", key: ";
+    if (req_type == kAppGetFgType || req_type == kAppGetBgType) {
+      const uint64_t *key_64 =
+          reinterpret_cast<const uint64_t *>(get_req.key.key_);
+      for (size_t i = 0; i < KV::kKeySize / sizeof(uint64_t); i++) {
+        ret << std::to_string(key_64[i]) << " ";
+      }
+    } else {
+      const uint64_t *key_64 =
+          reinterpret_cast<const uint64_t *>(put_req.key.key_);
+      for (size_t i = 0; i < KV::kKeySize / sizeof(uint64_t); i++) {
+        ret << std::to_string(key_64[i]) << " ";
+      }
+      ret << ", value: ";
+      const uint64_t *value_64 =
+          reinterpret_cast<const uint64_t *>(put_req.value.value_);
+      for (size_t i = 0; i < KV::kValueSize / sizeof(uint64_t); i++) {
+        ret << std::to_string(value_64[i]) << " ";
+      }
+    }
+    ret << "]";
+    return ret.str();
+  }
+};
+
+//resp size = size_t + kValueSize = 72 bytes
+enum class RespType : size_t { Success, Failure };
+struct wire_resp_t {
+  RespType resp_type;
+  KV::value_t value;
+
+  std::string to_string() const {
+    std::ostringstream ret;
+    ret << "[Value: ";
+    for (size_t i = 0; i < KV::kValueSize; i++) {
+      ret << std::to_string(value.value_[i]) << " ";
+    }
+    ret << "]";
+    return ret.str();
+  }
+};
+
+struct app_stats_t {
+  double mrps;       // Point request rate
+  double lat_us_50;  // Point request median latency
+  double lat_us_90;  // Point request 90th percentile latency
+  double lat_us_99;  // Point request 99th percentile latency
+  size_t pad[4];
+
+  app_stats_t() { memset(this, 0, sizeof(app_stats_t)); }
+
+  static std::string get_template_str() {
+    return "mrps lat_us_50 lat_us_90 lat_us_99";
+  }
+
+  std::string to_string() {
+    return std::to_string(mrps) + " " + std::to_string(lat_us_50) + " " +
+           std::to_string(lat_us_90) + " " + std::to_string(lat_us_99);
+  }
+
+  /// Accumulate stats
+  app_stats_t &operator+=(const app_stats_t &rhs) {
+    this->mrps += rhs.mrps;
+    this->lat_us_50 += rhs.lat_us_50;
+    this->lat_us_90 += rhs.lat_us_90;
+    this->lat_us_99 += rhs.lat_us_99;
+    return *this;
+  }
+};
+static_assert(sizeof(app_stats_t) == 64, "");
+
+// Per-thread application context
+class AppContext : public BasicAppContext {
+ public:
+  struct {
+    KV *kv = nullptr;
+  } server;
+
+  struct {
+    erpc::ChronoTimer tput_timer;  // Throughput measurement start
+    app_stats_t *app_stats;        // Common stats array for all threads
+
+    erpc::Latency get_latency;
+    erpc::Latency put_latency;
+
+    struct {
+      KV::key_t req_key;
+      uint64_t req_ts_;
+      erpc::MsgBuffer req_msgbuf_;
+      erpc::MsgBuffer resp_msgbuf_;
+    } window_[kAppMaxReqWindow];
+
+    erpc::FastRand fast_rand;
+    size_t num_resps_tot = 0;  // Total responses received (range & point reqs)
+  } client;
+};
+
+// Allocate request and response MsgBuffers
+void alloc_req_resp_msg_buffers(AppContext *c) {
+  for (size_t msgbuf_idx = 0; msgbuf_idx < FLAGS_req_window; msgbuf_idx++) {
+    c->client.window_[msgbuf_idx].req_msgbuf_ =
+        c->rpc_->alloc_msg_buffer_or_die(sizeof(wire_req_t));
+
+    c->client.window_[msgbuf_idx].resp_msgbuf_ =
+        c->rpc_->alloc_msg_buffer_or_die(sizeof(wire_resp_t));
+  }
+}
+
+#endif  // KV_H
